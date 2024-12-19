49db2ea6f0f76acba8de692c0badd82261b12447
Chompoo <chompoo190091@gmail.com>
2024-12-19 14:59:18 +0700

    listTable
    move todoList to this page for deploy

--- pages/list-table.tsx
+++ pages/list-table.tsx
@@ -1,13 +1,11 @@
 import { Box } from "@mui/material";
 import {ToDoList} from "@/components/TodoList";
-import { ToDoCard } from "@/components/TodoCard";
 
 export default function Home() {
   return (
     <>
       <Box sx={{padding:1}}>
-        {/* <ToDoList/> */}
-        <ToDoCard/>
+        <ToDoList/>
       </Box>
     </>
   );
